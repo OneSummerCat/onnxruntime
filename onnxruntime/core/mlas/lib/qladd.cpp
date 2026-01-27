21e2b401446e8b2165a7117e3f6001a2c530e0cc
“wangle” <wangle133@h-partners.com>
2026-01-27 10:52:31 +0800

    armv7a报错处理

--- onnxruntime/core/mlas/lib/qladd.cpp
+++ onnxruntime/core/mlas/lib/qladd.cpp
@@ -331,12 +331,35 @@ MlasQLinearAddKernelHelper(
             i8x8 = SUI::vget_high_i8(vc);
         }
         if (N & 4) {
+#if defined(__OHOS__) && defined(__arm__)
+            if (reinterpret_cast<uintptr_t>(OutputC) % 4 == 0) {
+                vst1_lane_u32_ex((uint32_t*)OutputC, SUI::vreinterpret_u32_i8(i8x8), 0, 8);
+            } else {
+                uint32_t temp;
+                memcpy(&temp, OutputC, sizeof(uint32_t));
+                vst1_lane_u32(&temp, SUI::vreinterpret_u32_i8(i8x8), 0);
+                memcpy(OutputC, &temp, sizeof(uint32_t));
+            }
+#else
             vst1_lane_u32_ex((uint32_t*)OutputC, SUI::vreinterpret_u32_i8(i8x8), 0, 8);
+#endif
             OutputC += 4;
             i8x8 = SUI::template vext_i8<4>(i8x8, i8x8);
         }
         if (N & 2) {
-            vst1_lane_u16_ex((uint16_t*)OutputC, SUI::vreinterpret_u16_i8(i8x8), 0, 8);
+#if defined(__OHOS__) && defined(__arm__)
+            if (reinterpret_cast<uintptr_t>(OutputC) % 4 == 0) {
+                // 对齐情况：使用原来的NEON指令
+                vst1_lane_u16_ex((uint16_t*)OutputC, SUI::vreinterpret_u16_i8(i8x8), 0, 8);
+            } else {
+                uint16_t temp_value;
+                memcpy(&temp_value, OutputC, sizeof(uint16_t));
+                vst1_lane_u16_ex(&temp_value, SUI::vreinterpret_u16_i8(i8x8), 0, 8);
+                memcpy(OutputC, &temp_value, sizeof(uint16_t));
+            }
+#else
+			vst1_lane_u16_ex((uint16_t*)OutputC, SUI::vreinterpret_u16_i8(i8x8), 0, 8);
+#endif
             OutputC += 2;
             i8x8 = SUI::template vext_i8<2>(i8x8, i8x8);
         }
--- onnxruntime/core/optimizer/qdq_transformer/selectors_actions/qdq_selector_action_transformer.h
+++ onnxruntime/core/optimizer/qdq_transformer/selectors_actions/qdq_selector_action_transformer.h
@@ -14,7 +14,7 @@
 namespace onnxruntime {
 
 inline constexpr bool QDQIsInt8Allowed() {
-#if !defined(MLAS_TARGET_ARM_ANY)
+#if !defined(MLAS_TARGET_ARM_ANY) || (defined(__OHOS__) && defined(__arm__))
   return false;
 #else
   return true;
